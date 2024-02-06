f0152506f4f3cc942523d6cc34f274bbd0df0146
Oliver LLuberes <olimail47@gmail.com>
2024-02-06 02:08:15 -0400

    saving requested stripe urls before user interaction to avoid waiting time

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -19,76 +19,95 @@ export default function AccountPage() {
     const email = auth.currentUser?.email;
 
     const [isPremium, setIsPremium] = useState(false);
+    const [checkoutUrl, setCheckoutUrl] = useState('');
+    const [portalUrl, setPortalUrl] = useState('');
 
     useEffect( () => {
       const checkPremium = async () => {
         const newPremiumStatus = auth.currentUser ? await getPremiumStatus(app) : false;
         setIsPremium(newPremiumStatus);
       }
-    
-      checkPremium();
+
+      checkPremium();  
     },[app, auth.currentUser]);
 
+
+    useEffect( () => {
+      if ( isPremium ) {
+        savePortalUrl();
+      } else{
+        saveCheckoutUrl();
+      }
+
+    }, [isPremium]);
 
     const signOut = () => {
       auth.signOut();
       router.push("/");
     }
-    
-    const upgradeToPremium = async () =>{
+
+    const saveCheckoutUrl = async () => {
       const priceId = 'price_1OeKEaKVbehzPhf3cYOxKV9E';
-      const checkoutUrl = await getCheckoutUrl(app, priceId);
+      const newCheckoutUrl = await getCheckoutUrl(app, priceId);
+      setCheckoutUrl(newCheckoutUrl);
+    }
+
+    const savePortalUrl = async () => {
+      const newPortalUrl = await getPortalUrl(app);
+      setPortalUrl(newPortalUrl);
+    }
+    
+    const upgradeToPremium = async () => {
       router.push(checkoutUrl);
     }
 
     const manageSubscription = async () => {
-       const portalUrl = await getPortalUrl(app);
        router.push(portalUrl);
     }
 
     const upgradeToPremiumButton = ( 
-        <Button
-          onClick={upgradeToPremium}
-          text="Upgrade To Premium"
-        />
-      );
+      <Button
+        onClick={upgradeToPremium}
+        text="Upgrade To Premium"
+        disabled={ checkoutUrl ? false : true }
+      />
+    );
 
-      const managePortalButton = (
-        <Button
-          onClick={manageSubscription}
-          text="Manage Subscription"
-        />
-      );
+    const managePortalButton = (
+      <Button
+        onClick={manageSubscription}
+        text="Manage Subscription"
+        disabled={ portalUrl ? false : true }
+      />
+    );
     
-      const signOutButton = (
-        <button
-          onClick={signOut}
-          className="hover:underline text-slate-500 hover:text-slate-300 text-lg text-center"
-        >
-          <div className="flex gap-2 items-center align-middle justify-center">
-            Sign Out
-          </div>
-        </button>
-      );
-    
-      const accountSummary = (
-        <div>
-          <div className="text-slate-500 mb-1">Signed in as {userName}</div>
-          <div className="text-slate-300 text-xl">{email}</div>
-        </div>
-      );
-    
-        const statusPanel = isPremium ? <PremiumPanel /> : <StandardPanel />;
-        const memberButton = isPremium ? managePortalButton : upgradeToPremiumButton;
-    
-      return (
-        <div className="flex flex-col justify-center items-center m-auto gap-8">
-          {accountSummary} 
-          {statusPanel}  
-          {memberButton}
-          {signOutButton}
-        </div>
-      );
+    const signOutButton = (
+      <button
+        onClick={signOut}
+        className="flex gap-2 items-center align-middle justify-center hover:underline text-slate-500 hover:text-slate-300 text-lg text-center"
+      >
+          Sign Out
+      </button>
+    );
+  
+    const accountSummary = (
+      <div>
+        <div className="text-slate-500 mb-1">Signed in as {userName}</div>
+        <div className="text-slate-300 text-xl">{email}</div>
+      </div>
+    );
+  
+      const statusPanel = isPremium ? <PremiumPanel /> : <StandardPanel />;
+      const memberButton = isPremium ? managePortalButton : upgradeToPremiumButton;
+  
+    return (
+      <div className="flex flex-col justify-center items-center m-auto gap-8">
+        {accountSummary} 
+        {statusPanel}  
+        {memberButton}
+        {signOutButton}
+      </div>
+    );
 
 
 }
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,16 +1,19 @@
 import React, { ReactNode } from 'react'
 
 type ButtonProps = {
-  text: string;
-  onClick: React.MouseEventHandler<HTMLButtonElement>;
-  icon?: ReactNode;
+  text : string;
+  onClick : React.MouseEventHandler<HTMLButtonElement>;
+  icon? : ReactNode;
+  disabled? : boolean;
 }
 
-export const Button = ({text, icon, onClick}: ButtonProps) => {
+export const Button = ({text, icon, onClick, disabled = false}: ButtonProps) => {
+  console.log("button " +  disabled)
   return (
     <button
         onClick={onClick}
-        className="flex gap-2 items-center align-middle justify-center bg-blue-600 p-4 px-6 text-lg rounded-lg hover:bg-blue-700 shadow-lg"
+        className="flex gap-2 items-center align-middle justify-center bg-blue-600 p-4 px-6 text-lg rounded-lg hover:bg-blue-700 shadow-lg disabled:bg-gray-500 disabled:opacity-70"
+        disabled={disabled}
     >
         {text}{icon}
     </button>
